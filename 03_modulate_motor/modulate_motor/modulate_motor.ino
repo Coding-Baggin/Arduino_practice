bb9abbe9d0737e00c9edb01f88d31d289f7b78df
Eum <taeho5114@gmail.com>
2026-01-06 11:34:50 +0900

    Add: Description

--- 03_modulate_motor/modulate_motor/modulate_motor.ino
+++ 03_modulate_motor/modulate_motor/modulate_motor.ino
@@ -1,38 +1,23 @@
-#include <Servo.h>
+const int echoPin = 8;
+const int trigPin = 9;
 
-Servo myServo; 
-const int servoPin = 9; // 서보 모터 연결 핀
-
-void setup() {
-  Serial.begin(9600); 
-  // 파이썬 숫자를 읽을 때 0.01초만 기다리도록 설정 (Flow 4번 반영)
-  Serial.setTimeout(10); 
-p
-  myServo.attach(servoPin);  
-  myServo.write(90); // 초기 각도 정중앙
-
-  // 파이썬이 부팅을 확인할 수 있도록 인사말 전송 (Flow 2번 반영)
-  Serial.println("Servo System Ready!"); 
+void main(){
+  Serial.begin(9600)
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    // 1. 숫자 읽기
-    int angle = Serial.parseInt(); 
-    
-    // 2. 버퍼에 남은 줄바꿈 문자 등 찌꺼기 청소 (Flow 5번 반영)
-    while(Serial.available() > 0) { 
-      Serial.read(); 
-    } 
+void loop(){
+  digitalWrite(trigPin, LOW);
+  delay(2);
+  digitalWrite(trigPin,HIGH);
+  delay(10);
+  digitalWrite(trigPin,LOW);
+  delay(2);
 
-    // 3. 각도 범위 확인 및 동작 (Flow 6번 반영)
-    if (angle >= 0 && angle <= 180) {
-      myServo.write(angle); 
-      delay(50); // 모터가 움직일 최소 시간 보장
-      
-      // 4. 결과를 파이썬으로 다시 보고 (Flow 6번 반영)
-      Serial.print("Moved to: "); 
-      Serial.println(angle);
-    }
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.017;
+  Serial.println(distance);
+  delay(100);  
+  
   }
-}
